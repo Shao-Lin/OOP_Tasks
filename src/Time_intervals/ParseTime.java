efc28cb76a98ce09793049964e39948426488e6b
Shao <117440556+Shao-Lin@users.noreply.github.com>
2023-12-02 08:58:16 +0300

    Parse data

--- src/Time_intervals/ParseTime.java
+++ src/Time_intervals/ParseTime.java
@@ -2,17 +2,28 @@ package Time_intervals;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class ParseTime {
     public static void main(String[] args) throws ParseException {
         parse();
     }
-    public static void parse() throws ParseException {
-        String dateStr = "2017-03-02 12:58";
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
-        String formated = dateFormat.format(dateFormat);
 
-        System.out.println(dateFormat);
+    public static void parse() throws ParseException {
+
+
+        String string = "2023.12.01 22:21";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy.MM.dd HH:mm");
+        LocalDateTime dateTime = LocalDateTime.parse(string, formatter);
+        System.out.println(dateTime);
+
+
+        ZonedDateTime z = ZonedDateTime.parse("2022-10-30T20:59:59.999Z");
+        System.out.println(z);
+
+        OffsetDateTime o = z.withZoneSameInstant(ZoneId.of("UTC-3")).toOffsetDateTime();
+        System.out.println(o);
 
     }
 }
